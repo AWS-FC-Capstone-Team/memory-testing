10988c2aecb2baccfe35748ff6e41296120811fc
anushkahebbar <56372418+anushkahebbar@users.noreply.github.com>
2022-11-01 22:27:57 +0530

    ramspeed tests with cpu affinity

--- test_scripts/firecrackerEnvironment/ramsmp_tests.bash
+++ test_scripts/firecrackerEnvironment/ramsmp_tests.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-stdbuf -o 0 ramsmp -b 1 2>&1 | tee firecracker/intmark_wr.log
-stdbuf -o 0 ramsmp -b 2 2>&1 | tee firecracker/intmark_rd.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 1 2>&1 | tee firecracker/intmark_wr.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 2 2>&1 | tee firecracker/intmark_rd.log
 
-stdbuf -o 0 ramsmp -b 4 2>&1 | tee firecracker/floatmark_wr.log
-stdbuf -o 0 ramsmp -b 5 2>&1 | tee firecracker/floatmark_rd.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 4 2>&1 | tee firecracker/floatmark_wr.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 5 2>&1 | tee firecracker/floatmark_rd.log
 
-stdbuf -o 0 ramsmp -b 3 -l 5 2>&1 | tee firecracker/intmem.log
-stdbuf -o 0 ramsmp -b 6 -l 5 2>&1 | tee firecracker/floatmem.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 3 -l 5 2>&1 | tee firecracker/intmem.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 6 -l 5 2>&1 | tee firecracker/floatmem.log
 
-stdbuf -o 0 ramsmp -b 13 2>&1 | tee firecracker/mmxmark_nt_wr.log
-stdbuf -o 0 ramsmp -b 14 2>&1 | tee firecracker/mmxmark_nt_rd.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 13 2>&1 | tee firecracker/mmxmark_nt_wr.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 14 2>&1 | tee firecracker/mmxmark_nt_rd.log
 
-stdbuf -o 0 ramsmp -b 16 2>&1 | tee firecracker/ssemark_nt_wr.log
-stdbuf -o 0 ramsmp -b 17 2>&1 | tee firecracker/ssemark_nt_rd.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 16 2>&1 | tee firecracker/ssemark_nt_wr.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 17 2>&1 | tee firecracker/ssemark_nt_rd.log
 
-stdbuf -o 0 ramsmp -b 15 -l 5 2>&1 | tee firecracker/mmxmem.log
-stdbuf -o 0 ramsmp -b 18 -l 5 2>&1 | tee firecracker/ssemem.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 15 -l 5 2>&1 | tee firecracker/mmxmem.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 18 -l 5 2>&1 | tee firecracker/ssemem.log
--- test_scripts/ramsmp_tests.bash
+++ test_scripts/ramsmp_tests.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-stdbuf -o 0 ramsmp -b 1 2>&1 | tee baremetal/intmark_wr.log
-stdbuf -o 0 ramsmp -b 2 2>&1 | tee baremetal/intmark_rd.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 1 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/intmark_wr.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 2 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/intmark_rd.log
 
-stdbuf -o 0 ramsmp -b 4 2>&1 | tee baremetal/floatmark_wr.log
-stdbuf -o 0 ramsmp -b 5 2>&1 | tee baremetal/floatmark_rd.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 4 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/floatmark_wr.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 5 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/floatmark_rd.log
 
-stdbuf -o 0 ramsmp -b 3 -l 5 2>&1 | tee baremetal/intmem.log
-stdbuf -o 0 ramsmp -b 6 -l 5 2>&1 | tee baremetal/floatmem.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 3 -l 5 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/intmem.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 6 -l 5 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/floatmem.log
 
-stdbuf -o 0 ramsmp -b 13 2>&1 | tee baremetal/mmxmark_nt_wr.log
-stdbuf -o 0 ramsmp -b 14 2>&1 | tee baremetal/mmxmark_nt_rd.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 13 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/mmxmark_nt_wr.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 14 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/mmxmark_nt_rd.log
 
-stdbuf -o 0 ramsmp -b 16 2>&1 | tee baremetal/ssemark_nt_wr.log
-stdbuf -o 0 ramsmp -b 17 2>&1 | tee baremetal/ssemark_nt_rd.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 16 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/ssemark_nt_wr.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 17 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/ssemark_nt_rd.log
 
-stdbuf -o 0 ramsmp -b 15 -l 5 2>&1 | tee baremetal/mmxmem.log
-stdbuf -o 0 ramsmp -b 18 -l 5 2>&1 | tee baremetal/ssemem.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 15 -l 5 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/mmxmem.log
+stdbuf -o 0 taskset -c 0-1 ramsmp -b 18 -l 5 2>&1 | tee ../test_outputs/ramsmp_outputs/baremetal/ssemem.log
